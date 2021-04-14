d476d9286d2631c05717287cf6d6e018f04d4cd0
Raphaël Deknop <raphael.deknop@ulb.be>
2021-04-14 21:28:37 +0200

    Added comments to R script

--- plot.r
+++ plot.r
@@ -1,18 +1,25 @@
-df <- read.csv('hist.csv');
+#Loading the CSV
+df <- read.csv('activity_history.csv');
 
+#Converting the time units
 df$HRTIME <- as.POSIXct(df$TIME/1000, origin = "1970-01-01", tz="UTC")
 
+#Storing the columns in x and y vectors for easier reading & plotting
 x <- df$HRTIME;
 y <- df$PLAYERS;
 
+#Defining the properties of the plot image
 png("plot.png", width=1800, height=900)
 
+#plot
 p <- plot(df$HRTIME, df$PLAYERS, 
      main="Decentraland players across time", 
      sub="Data aggregated and analysed by Rphad", 
      xlab = "Time (UTC)",
      ylab = "Number of players")
 
+#Adding lines between points for better reading
 lines(x[order(x)], y[order(x)])
 
+#Closing device, thus saving the file
 dev.off()
